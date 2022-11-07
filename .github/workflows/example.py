805738a47a7b2425e5c2c4efb1676d0e6058379e
cprajwowksi <115100489+cprajwowksi@users.noreply.github.com>
2022-11-07 21:53:28 +0100

    Update example.py

--- .github/workflows/example.py
+++ .github/workflows/example.py
@@ -4,24 +4,10 @@ def suma3(a,b,c):
   result = a + b + c
   return result;
 class TestStringMethods(unittest.TestCase):
-
-    def test_upper(self):
-        self.assertEqual('foo'.upper(), 'FOO')
     
     def test_suma3(self):
         result = suma3(10, 2, 3)
         self.assertEqual(result, 15)
-
-    def test_isupper(self):
-        self.assertTrue('FOO'.isupper())
-        self.assertFalse('Foo'.isupper())
-
-    def test_split(self):
-        s = 'hello world'
-        self.assertEqual(s.split(), ['hello', 'world'])
-        # check that s.split fails when the separator is not a string
-        with self.assertRaises(TypeError):
-            s.split(2)
 
 if __name__ == '__main__':
     unittest.main()

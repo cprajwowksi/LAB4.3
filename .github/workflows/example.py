80a02d2a83aed4a21620d720217e3d1697486526
cprajwowksi <115100489+cprajwowksi@users.noreply.github.com>
2022-11-07 21:35:21 +0100

    Update example.py

--- .github/workflows/example.py
+++ .github/workflows/example.py
@@ -1,6 +1,11 @@
 
-def main():
-  print("Hellow from Github Actions!")
+import unittest
+import dotesta
+
+class TestDotesta(unittest.TestCase):
+  def test_suma3(self):
+        result = suma3.add(10, 2, 3)
+        self.assertEqual(result, 15)
 
 
 if __name__ == '__main__':

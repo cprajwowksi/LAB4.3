ae9d3443a911c8bc7bb1498ffa5d136eb0e31307
cprajwowksi <115100489+cprajwowksi@users.noreply.github.com>
2022-11-07 21:36:18 +0100

    Update example.py

--- .github/workflows/example.py
+++ .github/workflows/example.py
@@ -3,7 +3,7 @@ import unittest
 import dotesta
 
 class TestDotesta(unittest.TestCase):
-  def test_suma3(self):
+  def main(self):
         result = suma3.add(10, 2, 3)
         self.assertEqual(result, 15)
 
